85a75aec33c2fe147d599b9c6d31d0dd00a4077c
Kondo Takeo <takeoverjp@gmail.com>
2021-10-03 22:30:28 +0900

    AtCoder Beginners Selection 2

---
+++ ABC086A.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int64_t a, b;
+  cin >> a >> b;
+  if (a % 2 && b % 2) {
+    cout << "Odd" << endl;
+  } else {
+    cout << "Even" << endl;
+  }
+  return 0;
+}
