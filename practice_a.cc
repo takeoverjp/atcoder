1d6dcc3669ba0926ac3fb9a9bc0216df13dde477
Kondo Takeo <takeoverjp@gmail.com>
2021-10-03 22:29:47 +0900

    AtCoder Beginners Selection 1

---
+++ practice_a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int64_t a;
+  cin >> a;
+  int64_t b, c;
+  cin >> b >> c;
+  string s;
+  cin >> s;
+  cout << a + b + c << " " << s << endl;
+  return 0;
+}
