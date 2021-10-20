3952ff1c7b5a15d25b50f0757ce84a38b4be306d
Kondo Takeo <takeoverjp@gmail.com>
2021-10-21 07:23:56 +0900

    Educational DP Contest F: improve performance by using int dp

--- dp_f.cc
+++ dp_f.cc
@@ -5,26 +5,40 @@ int main() {
   cin >> s >> t;
   int slen = s.length();
   int tlen = t.length();
-  vector<string> dp(tlen + 1);
-  vector<string> work(tlen + 1);
+  vector<vector<int>> dp(tlen + 1, vector<int>(slen + 1));
   for (int i = 1; i <= slen; i++) {
     for (int j = 1; j <= tlen; j++) {
       if (s[i - 1] == t[j - 1]) {
-        work[j] = dp[j - 1] + s[i - 1];
+        dp[j][i] = dp[j - 1][i - 1] + 1;
       } else {
-        if (dp[j].length() > work[j - 1].length()) {
-          work[j] = dp[j];
-        } else {
-          work[j] = work[j - 1];
-        }
+        dp[j][i] = max(dp[j - 1][i], dp[j][i - 1]);
       }
     }
-    std::swap(dp, work);
-    // for (const auto c : dp) {
-    //   cout << c << " ";
+    // for (const auto &row : dp) {
+    //   for (const auto val : row) {
+    //     cout << val << " ";
+    //   }
+    //   cout << endl;
     // }
-    // cout << endl;
   }
-  cout << dp[tlen] << endl;
+  int len = dp[tlen][slen];
+  string ret;
+  ret.resize(len);
+  int i = slen, j = tlen;
+  while (len > 0) {
+    if (j >= 1 && dp[j][i] == dp[j - 1][i]) {
+      j--;
+      continue;
+    }
+    if (i >= 1 && dp[j][i] == dp[j][i - 1]) {
+      i--;
+      continue;
+    }
+    j--;
+    i--;
+    len--;
+    ret[len] = s[i];
+  }
+  cout << ret << endl;
   return 0;
 }
