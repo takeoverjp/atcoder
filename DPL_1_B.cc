ba5f64b7183441549ec56a05a858ff23b8124944
Kondo Takeo <takeoverjp@gmail.com>
2021-12-04 21:46:12 +0900

    DPL_1: B - 0-1 ナップサック問題

---
+++ DPL_1_B.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (ll i = 0; i < (n); ++i)
+using namespace std;
+typedef long long ll;
+static vector<vector<ll>> dp;
+
+int main() {
+  ll N, W;
+  cin >> N >> W;
+  vector<ll> v(N, 0);
+  vector<ll> w(N, 0);
+  rep(i, N) { cin >> v[i] >> w[i]; }
+  dp.resize(N + 1);
+  rep(i, N + 1) { dp[i].assign(W + 1, 0); }
+  rep(i, N) {
+    for (ll j = 1; j <= W; j++) {
+      if (j < w[i]) {
+        dp[i + 1][j] = dp[i][j];
+      } else {
+        dp[i + 1][j] = max(dp[i][j], dp[i][j - w[i]] + v[i]);
+      }
+    }
+  }
+  // rep(i, N + 1) {
+  //   rep(j, W + 1) { cout << dp[i][j] << " "; }
+  //   cout << endl;
+  // }
+  cout << dp[N][W] << endl;
+  return 0;
+}
